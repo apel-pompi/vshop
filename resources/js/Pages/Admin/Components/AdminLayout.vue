84f07a1ddf7258d7deb09db6994c636da409579a
apel-pompi <apel.pompi@gmail.com>
2024-10-21 19:34:20 +0600

    first commit

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- Navbar start -->
+         <Navbar/>
+        <!-- Navbar End -->
+
+        <!-- Sidebar start -->
+        <Sidebar/>
+        
+        <!-- Sidebar End -->
+
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+</script>
